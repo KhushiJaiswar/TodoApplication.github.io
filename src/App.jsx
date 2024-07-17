6499a6a8e8a96664cfc24d69e4ed02240adbcbae
Khushi Jaiswar <khushijaiswar77049@gmail.com>
2024-07-17 14:40:53 +0530

    TodoApplication

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import AppName from "./components/AppName";
+import AddTodo from "./components/AddTodo";
+import TodoItems from "./components/TodoItems";
+import WelcomeMessage from "./components/WelcomeMessage";
+import "./App.css";
+import { useState } from "react";
+
+function App() {  
+  const initialTodoItems = ([]);
+
+  const [todoItems, setTodoItems] = useState(initialTodoItems);
+
+  const handleNewItem = (itemName, itemDueDate) => {
+    const newTodoItems =[
+    ...todoItems, 
+    { name: itemName, dueDate: itemDueDate,}
+    ];
+    setTodoItems(newTodoItems);
+  };
+
+  const handleDeleteItem = (todoItemName) => {
+    const newTodoItems = todoItems.filter (item => item.name !== todoItemName);
+    setTodoItems(newTodoItems);
+  };
+  
+
+  return ( 
+
+  <center className="todo-container">
+    <AppName></AppName>
+    <AddTodo onNewItem={handleNewItem}></AddTodo>
+    {todoItems.length === 0 && <WelcomeMessage></WelcomeMessage>}
+    <TodoItems todoItems={todoItems} onDeleteClick={handleDeleteItem}></TodoItems>
+   
+  </center>
+  );
+}
+
+export default App;
